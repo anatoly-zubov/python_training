ab0eb24d85d377a8d4d3cc991f0b0d146e897a9b
Anatoly <anatoliiblik@yandex.ru>
2020-08-28 12:59:38 +0300

    Фикс багов

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ class Application:
 
     def login(self, username, password):
         wd = self.wd
-        self.open_home_page(wd)
+        self.open_home_page()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
