f1ec6ce340b17014e02645bcca7e48d8c9f056be
Anatoly <anatoliiblik@yandex.ru>
2020-09-02 16:50:54 +0300

    Добавлено обращение к классу для упраления сессиями при добавлении контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,11 +10,11 @@ def app(request):
     return fixture
 
 def untitled_test_case(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Group_contact(firstname ="", lastname="", homephone="", mobile="", email=""))
-    app.logout()
+    app.session.logout()
 
 def test_untitled_test_case(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Group_contact(firstname="Anatolii", lastname="Zubov", homephone="222-22-22", mobile="846-743", email="gasgfg"))
-    app.logout()
+    app.session.logout()
