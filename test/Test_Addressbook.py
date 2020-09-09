248d6b6fc1340ffda9b7c34aea749a1caa4e3481
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 13:12:23 +0300

    Создание класса удаления Группы

--- test/Test_Addressbook.py
+++ test/Test_Addressbook.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
-def test_empty_addressbook(app):
+def test_empty_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
     
-def test_addressbook(app):
+def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="newgroup", header="grouplogo", footer="groupfooter"))
     app.session.logout()
