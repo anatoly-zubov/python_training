fff587fd8fa85ad32ef197882e94ffc630537b33
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 17:58:51 +0300

    Добавлина проверка предусловий авторизации

--- test/Test_Addressbook.py
+++ test/Test_Addressbook.py
@@ -3,6 +3,7 @@ from model.group import Group
 
 def test_empty_add_group(app):
     app.group.create(Group(name="", header="", footer=""))
+
     
 def test_add_group(app):
     app.group.create(Group(name="newgroup", header="grouplogo", footer="groupfooter"))
