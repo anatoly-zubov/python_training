1cf4edba8b577de23b508f834174bbe3ef5c727b
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 17:31:34 +0300

    Добавлина проверка валидности фикстуры. Логин и логаут выполняются один раз

--- test/Test_Addressbook.py
+++ test/Test_Addressbook.py
@@ -2,12 +2,9 @@
 from model.group import Group
 
 def test_empty_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
     
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="newgroup", header="grouplogo", footer="groupfooter"))
-    app.session.logout()
+
 
