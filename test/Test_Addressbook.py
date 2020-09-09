d0be4c7477e161a4b31b8c597e6c07017c497cb7
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 13:42:52 +0300

    Добавлен класс удаления контактов

--- test/Test_Addressbook.py
+++ test/Test_Addressbook.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_empty_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
