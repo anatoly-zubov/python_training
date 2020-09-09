37739dcb0d786a9e2830c4c148f0e411ee2f3ace
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 13:18:02 +0300

    Создание общей фикстуры для всех классов и работа в одной сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group_contact import Group_contact
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
