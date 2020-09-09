c3e62254d97fecfdc1873f3e5ba963bdfe9dec6f
Anatoly <anatoliiblik@yandex.ru>
2020-09-09 18:04:45 +0300

    Добавлина проверка валидности фикстуры и проверка состояния сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,10 @@
 from model.group_contact import Group_contact
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
+
     app.contact.create_contact(Group_contact(firstname ="", lastname="", homephone="", mobile="", email=""))
-    app.session.logout()
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
+
     app.contact.create_contact(Group_contact(firstname="Anatolii", lastname="Zubov", homephone="222-22-22", mobile="846-743", email="gasgfg"))
-    app.session.logout()
+
