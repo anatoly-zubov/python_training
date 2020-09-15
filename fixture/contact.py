b8349715d8237ab231f0a475cacdc2b654697b76
Anatoly <anatoliiblik@yandex.ru>
2020-09-15 16:05:56 +0300

    Доработка теста удаления контактов. Добавление проверки предусловия

--- fixture/contact.py
+++ fixture/contact.py
@@ -53,3 +53,7 @@ class ContactHelper:
     def return_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
+
+    def count(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,6 @@
-
+from model.group_contact import Group_contact
 
 def test_delete_first_contact(app):
-
+    if app.contact.count() == 0:
+        app.contact.create_contact(Group_contact(firstname="Name"))
     app.contact.delete_first_contact()
