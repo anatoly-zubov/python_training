0a41a1d4b8d9a841fbb6abb65b743e5c49d3f56b
Anatoly <anatoliiblik@yandex.ru>
2020-09-23 14:14:37 +0300

    Сортировка списков перед сравнением

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
+
 def test_empty_add_group(app):
+    group = Group(name="", header="", footer="")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_group(app):
+    group = Group(name="newgroup", header="grouplogo", footer="groupfooter")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="newgroup", header="grouplogo", footer="groupfooter"))
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,18 +5,21 @@ from fixture.precond_verifi import precond_verifi_group
 def test_modify_group_name(app):
     precond_verifi_group(app)
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New_Group"))
+    group = Group(name="New_Group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0]= group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
 
-def test_modify_group_header(app):
-    precond_verifi_group(app)
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New_Header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups)  == len(new_groups)
+#def test_modify_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    app.group.modify_first_group(Group(header="New_Header"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups)  == len(new_groups)
 
 
