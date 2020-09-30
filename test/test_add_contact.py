8acbe4b57b87f9d57fee3b2058dae7e0e5c880b5
Anatoly <anatoliiblik@yandex.ru>
2020-09-30 11:07:05 +0300

    Проверка равенства объектов и списков

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from model.group_contact import Group_contact
 
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#    contact = Group_contact(firstname="", lastname="", mobile="", email="")
-#    app.contact.create_contact(contact)
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
-#    old_contacts.append(contact)
- #   assert sorted(old_contacts) == sorted(new_contacts)
+def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Group_contact(firstname="", lastname="", mobile="", email="")
+    app.contact.create_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Group_contact.id_or_max) == sorted(new_contacts, key=Group_contact.id_or_max)
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
