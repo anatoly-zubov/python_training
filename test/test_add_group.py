96ea61fa6307e240d28dfe0ad30e8bae1a1b3bb6
Anatoly <anatoliiblik@yandex.ru>
2020-09-30 11:36:11 +0300

    Кэширование и хэширование групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
+"""
 def test_empty_add_group(app):
     group = Group(name="", header="", footer="")
     old_groups = app.group.get_group_list()
@@ -10,14 +10,15 @@ def test_empty_add_group(app):
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+"""
 
 
 def test_add_group(app):
     group = Group(name="newgroup", header="grouplogo", footer="groupfooter")
     old_groups = app.group.get_group_list()
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
