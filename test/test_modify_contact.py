6182e57b598b595a4253c180e323560674f8193f
Anatoly <anatoliiblik@yandex.ru>
2020-09-30 11:20:14 +0300

    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп и контактов.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,6 +5,10 @@ from fixture.precond_verifi import precond_verifi_contact
 def test_modify_contact_firstname(app):
     precond_verifi_contact(app)
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Group_contact(firstname="New_Name"))
+    contact = Group_contact(lastname="New_Name")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Group_contact.id_or_max) == sorted(new_contacts, key=Group_contact.id_or_max)
